a496f35b794d842a7739514fa844a3716b6a9cc1
NicoDVK <71187149+NicoDVKa@users.noreply.github.com>
2020-10-19 22:04:52 -0300

    Creacion de la tabla de hash.
    Ademas agrego el metodo insertar(empleado).

---
+++ HashTable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HT
+{
+    class HashTable
+    {
+        private int tamaño;
+        private ArrayList[] tabla;
+
+        public HashTable(int tamaño)
+        {
+            this.tamaño = tamaño;
+            this.tabla = new ArrayList[tamaño];
+        }
+
+        //Dispersion abierta
+        public void insertar(Empleado empleado)
+        {
+            int clave = empleado.DNI % tamaño;
+
+            if (tabla[clave]==null)
+            {
+                tabla[clave] = new ArrayList();
+                tabla[clave].Add(empleado);
+            }
+            else
+            {
+                tabla[clave].Add(empleado);
+            }
+            
+        }
+
+        public void mostrarTabla() //Lo use para corroborar que funcionara bien
+        {
+            for (int i = 0; i<tamaño ; i++)
+            {
+                Console.WriteLine("Tabla en la posicion {0}",i);
+                
+                if (tabla[i]!=null)
+                {
+                    foreach (Empleado x in tabla[i])
+                    {
+                        Console.WriteLine(x.DNI);
+                    }
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,16 +7,24 @@ namespace HT
         static void Main(string[] args)
         {
             Empleado empleado1 = new Empleado("Nicolas Herlan",01,43400885);
-            Empleado empleado2 = new Empleado("Lionel Farias",02,44022345);
+            Empleado empleado2 = new Empleado("Lionel Farias",02,44022344);
             Empleado empleado3 = new Empleado("Francisco Iturburu",03,41400379);
-            Empleado empleado4 = new Empleado("Joaquin Brilz",04,45400985);
-            Empleado empleado5 = new Empleado("Bruno Hoffmann",05,46400345);
-            
-            Console.WriteLine(empleado1.NumEmpleado + " " + empleado1.DNI + " " + empleado1.Nombre);
-            Console.WriteLine(empleado2.NumEmpleado + " " + empleado2.DNI + " " + empleado2.Nombre);
-            Console.WriteLine(empleado3.NumEmpleado + " " + empleado3.DNI + " " + empleado3.Nombre);
-            Console.WriteLine(empleado4.NumEmpleado + " " + empleado4.DNI + " " + empleado4.Nombre);
-            Console.WriteLine(empleado5.NumEmpleado + " " + empleado5.DNI + " " + empleado5.Nombre);
+            Empleado empleado4 = new Empleado("Joaquin Brilz",04,45400982);
+            Empleado empleado5 = new Empleado("Bruno Hoffmann",05,46400343);
+
+            HashTable tabla = new HashTable(5);
+
+            tabla.insertar(empleado1);
+            tabla.insertar(empleado2);
+            tabla.insertar(empleado3);
+            tabla.insertar(empleado4);
+            tabla.insertar(empleado5);
+
+            tabla.mostrarTabla();
+
+            Console.WriteLine("Presione un tecla para salir...");
+            Console.ReadKey(true);
+
         }
     }
 }
