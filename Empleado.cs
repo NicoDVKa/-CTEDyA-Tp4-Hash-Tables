7e0f91c6d36b6c8c2c47af64c333216e351f310c
NicoDVK <71187149+NicoDVKa@users.noreply.github.com>
2020-10-19 21:28:00 -0300

    Creacion e implementacion de la clase empleado

---
+++ Empleado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace HT
+{
+    class Empleado
+    {
+        private string nombre;
+        private int numEmpleado;
+        private int dni;
+
+        public Empleado(string nombre, int numEmpleado, int dni)
+        {
+            this.nombre = nombre;
+            this.numEmpleado = numEmpleado;
+            this.dni = dni;
+        }
+        
+        public int DNI
+        {
+            get { return this.dni; }        
+        }
+        
+        public string Nombre
+        {
+            get { return this.nombre; }
+        }
+        
+        public int NumEmpleado
+        {
+            get { return this.numEmpleado; }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace HT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Empleado empleado1 = new Empleado("Nicolas Herlan",01,43400885);
+            Empleado empleado2 = new Empleado("Lionel Farias",02,44022345);
+            Empleado empleado3 = new Empleado("Francisco Iturburu",03,41400379);
+            Empleado empleado4 = new Empleado("Joaquin Brilz",04,45400985);
+            Empleado empleado5 = new Empleado("Bruno Hoffmann",05,46400345);
+            
+            Console.WriteLine(empleado1.NumEmpleado + " " + empleado1.DNI + " " + empleado1.Nombre);
+            Console.WriteLine(empleado2.NumEmpleado + " " + empleado2.DNI + " " + empleado2.Nombre);
+            Console.WriteLine(empleado3.NumEmpleado + " " + empleado3.DNI + " " + empleado3.Nombre);
+            Console.WriteLine(empleado4.NumEmpleado + " " + empleado4.DNI + " " + empleado4.Nombre);
+            Console.WriteLine(empleado5.NumEmpleado + " " + empleado5.DNI + " " + empleado5.Nombre);
         }
     }
 }
